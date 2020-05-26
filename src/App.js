081307eec87cbb774ded79afe9f1adb6b36056eb
Liz Ruelas <liz.ruelas@tcs.com>
2020-05-25 20:27:41 -0500

    add

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component }  from 'react';
 import './App.css';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag'
 
-function App() {
+
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			searchString: '',
+			jobs: []
+		};
+		this.handleChange = this.handleChange.bind(this);
+		
+	}
+
+	componentInput() {
+		this.setState({
+      jobs: this.props.data.jobs
+		});
+	}
+  
+  handleChange = event => {
+		this.setState({
+      searchString: event.target.value
+		});
+	};
+	
+ render() {
+	const { loading , error , jobs } = this.props.data
+	if (loading) return 'Cargando...';
+  if (error) return `Error! ${error.message}`;
+
+	let _jobs = jobs;
+  let search = this.state.searchString.trim().toLowerCase();
+	if (search.length > 0) {
+		_jobs = _jobs.filter(function(job) {
+			return job.title.toLowerCase().match(search);
+		});
+	}
+
+	
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+
+		<div className="container">
+			<input  
+					type="text"
+					value={this.state.searchString}
+					onChange={this.handleChange}
+					placeholder="Buscar">
+			</input>
+		</div>
+		<ul>
+			{_jobs.map(l => {
+				return (
+					<li>
+						{l.title} 
+					</li>
+				);
+			})}
+		</ul>
+	</React.Fragment>
   );
+ }
 }
 
-export default App;
+const GET_JOBS = gql`
+{
+  jobs {
+    title
+  }
+}
+`;
+const AppWithData = graphql(
+  GET_JOBS,
+  {}
+)(App)
+
+
+
+export default AppWithData;
